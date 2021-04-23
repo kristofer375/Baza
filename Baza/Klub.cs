a910a99a26784cfd86b274fb03985166d9dcfa73
Kristofer <skipperk@interia.pl>
2021-04-23 20:00:02 +0200

    Przypisanie Bazy

---
+++ Baza/Klub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza
+{
+    public class Klub
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Ikona { get; set; }
+        public string Skrot { get; set; }
+
+        public string Kluby
+        {
+            get
+            {
+                return $"{ Nazwa } { Skrot }";
+            }
+        }
+    }
+}
---
+++ Baza/SQLiteDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza
+{
+    public class SQLiteDataAccess
+    {
+        public static List<Klub> LoadKlub()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Klub>("select * from Kluby", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        //public static void SaveKlub(Klub klub)
+        //{
+        //    using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+        //    {
+        //        cnn.Execute("insert into Kluby (Nazwa, Ikona, Skrot) values (@Nazwa, NULL, @Skrot)", klub);
+        //    }
+        //}
+        public static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
